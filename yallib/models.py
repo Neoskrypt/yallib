33b2dd49658b1d4447821a332d2877c7690217e6
GenBor <Generalovboris@outlook.com>
2018-05-27 20:05:42 +0300

    fix bugs table Author

--- yallib/models.py
+++ yallib/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from datetime import datetime
 class BaseModel(models.Model):
     created = models.DateTimeField(default=datetime.now, blank=True)
-    changed = models.DateTimeField(default=datetime.now, blank=True)
+    changed = models.DateTimeField(auto_now=True)
     class Meta:
         abstract = True
 class Author(BaseModel):
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     date_birth = models.DateField()
 class Publication(BaseModel):
     caption = models.CharField(max_length=100)
