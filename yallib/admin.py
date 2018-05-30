235e182b5043c5bde354370c6692741ed0760a31
GenBor <Generalovboris@outlook.com>
2018-05-30 17:08:02 +0300

    create models f/authorizations

---
+++ yallib/admin.py
@@ -0,0 +1,10 @@
+from django.contrib.admin import ModelAdmin
+from django.contrib.admin import register
+
+
+import yallib.models as models
+
+
+@register(models.Author)
+class AuthorAdmin(ModelAdmin):
+        pass
