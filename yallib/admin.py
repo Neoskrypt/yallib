44bb1bb31597aa18f96a6df1e68a6377fa2e422e
GenBor <Generalovboris@outlook.com>
2018-05-31 09:27:51 +0300

    change settings path db

--- yallib/admin.py
+++ yallib/admin.py
@@ -8,3 +8,6 @@ import yallib.models as models
 @register(models.Author)
 class AuthorAdmin(ModelAdmin):
         pass
+@register(models.User)
+class UserAdmin(ModelAdmin):
+    pass
--- yallib/settings.py
+++ yallib/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'yallib.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('/home/boris/MEGA/Python/Python_Learning/dbYallib', 'db.sqlite3'),
     }
 }
 
