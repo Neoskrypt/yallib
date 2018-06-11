9ce9cfbe8acd33ae190c4168c6685c54983f4310
GenBor <Generalovboris@outlook.com>
2018-06-11 10:20:24 +0300

    static files

--- yallib/settings.py
+++ yallib/settings.py
@@ -146,7 +146,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'templates', 'static'),
+    os.path.join(BASE_DIR,),
 )
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
