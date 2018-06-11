b3876ab1887fb97006d236394a35cbffa4a3af04
GenBor <Generalovboris@outlook.com>
2018-06-11 10:12:12 +0300

    static files

--- yallib/settings.py
+++ yallib/settings.py
@@ -143,10 +143,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'templates', 'static'),
 )
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
