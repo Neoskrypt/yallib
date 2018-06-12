c2a32aec9ff6b109ad4f22dbd337ed59bdcbde36
GenBor <Generalovboris@outlook.com>
2018-06-12 12:37:42 +0300

    fix debugs

--- yallib/views.py
+++ yallib/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as auth_login
 from django.template.context_processors import csrf
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import auth
@@ -10,13 +11,13 @@ from yallib.models import Author
 def get_authors(request):
     return render_to_response('\
     authors.html', {"Authors": Author.objects.all(), "\
-    username": auth.get_user(request).username})
+    username": auth.get_user(request).get_username()})
 
 
 def get_author(request, id):
     return render_to_response('\
     authors.html', {"Authors": Author.objects.filter(pk=id), "\
-    username": auth.get_user(request).username})
+    username": auth.get_user(request).get_username()})
 
 
 @csrf_protect
@@ -28,7 +29,7 @@ def login(request):
         password = request.POST.get("password", "")
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            auth_login(request, user)
             return redirect("/")
         else:
             args["login_error"] = "User not found!!!"
